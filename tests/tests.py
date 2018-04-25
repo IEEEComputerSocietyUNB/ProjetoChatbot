c4c721044a31f3fd7348067f949055e05449ecfc
Alexandre Augusto <alexandrebarbaruiva@users.noreply.github.com>
2018-04-25 13:20:48 -0300

    Create tests.py

---
+++ tests/tests.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+from bot.bot import Chatbot
+from bot.bot import retrieve_token
+
+
+class TestBot(unittest.TestCase):
+
+    def setUp(self):
+        self.bot = Chatbot(retrieve_token())
+
+    def test_nothing(self):
+        """
+        Check if testing is really ocurring
+        """
+        pass
+
+    def test_if_bot_is_unbchatbot(self):
+        """
+        Check if bot being initialized is truly the unbchatbot
+        """
+        self.assertEqual(self.bot.verify_bot(), ('unbchatbot', 330147863))
+
+    def test_if_bot_logging_works(self):
+        """
+        Check for bot logging
+        """
+        # self.assertEqual(self.bot.make_log(), 'test')
+        pass
+
+    def test_if_start_command_works(self):
+        """
+        Check for start command veracity
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
