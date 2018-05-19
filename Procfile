318f6d63d8110982eeb1287efd853018f6a8fb54
alexandrebarbaruiva <lexander.santos@gmail.com>
2018-05-18 21:59:32 -0300

    Add bot to test heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python bot/bot.py 
+web: python bot/herokubot.py 
---
+++ bot/herokubot.py
@@ -0,0 +1,44 @@
+import logging
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def start(bot, update):
+    update.effective_message.reply_text("Hi!")
+
+
+def echo(bot, update):
+    update.effective_message.reply_text(update.effective_message.text)
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = "Your token from @Botfather"
+    NAME = "The name of your app on Heroku"
+
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+
+    # Start the webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.idle()
