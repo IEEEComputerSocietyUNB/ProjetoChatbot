28a2e38f9e3dad973fcae7160a212cf75d9e1f17
alexandrebarbaruiva <lexander.santos@gmail.com>
2018-06-11 08:46:10 -0300

    Add method to remove pronouns and articles

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -25,18 +25,42 @@ class TestBotCommunication(unittest.TestCase):
 
     def test_if_comm_removes_accents_and_upper_letters(self):
         """
-        Check if communication method cleans
+        Check if communication method cleans.
         """
         # Remove upper letters
         self.assertEqual(self.comm.clean("ABCDEF"), "abcdef")
 
-    def test_if_comm_removes_abbreviation(self):
-        self.assertEqual(self.comm.clean("cadê vc"), "cadê você")
-        self.assertEqual(self.comm.clean("vc é um chato"), "você é um chato")
+    def test_if_comm_raises_error_when_abbr_not_found(self):
+        with self.assertRaises(TypeError):
+            self.comm.switch_abbreviations("teste", files="fail.txt")
 
     def test_if_comm_raises_error_when_abbr_not_found(self):
         with self.assertRaises(FileNotFoundError):
-            self.comm.clean("teste", file="fail.txt")
+            self.comm.switch_abbreviations("teste", files=["fail.txt"])
+
+    def test_comm_switches_abbreviations(self):
+        self.assertEqual(
+            self.comm.switch_abbreviations("vc está aki?"),
+            "você está aqui?"
+        )
+
+    def test_if_comm_raises_error_when_rm_not_found(self):
+        with self.assertRaises(TypeError):
+            self.comm.remove_words("teste", files="fail.txt")
+
+    def test_if_comm_raises_error_when_rm_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            self.comm.remove_words("teste", files=["fail.txt"])
+
+    def test_comm_removes_pronouns_and_articles(self):
+        self.assertEqual(
+            self.comm.remove_words("conte uma piada"),
+            "conte piada"
+        )
+
+    def test_if_comm_clean_returns_clean_messages(self):
+        self.assertEqual(self.comm.clean("cadê vc"), "cadê você")
+        self.assertEqual(self.comm.clean("vc é um chato"), "você é chato")
 
 
 if __name__ == '__main__':
