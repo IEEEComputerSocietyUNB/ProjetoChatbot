07a71a366a994a1325de92d623da31337f0e0636
tapumar@gmail.com <tapumar@gmail.com>
2018-06-27 22:27:27 -0100

    Improve tests and add more code to linter (WIP)
    Co-authored-by: alexandrebarbaruiva <alexandrebarbaruiva@gmail.com>

--- tests/test_application.py
+++ tests/test_application.py
@@ -15,14 +15,20 @@ from bot.application import Application, retrieve_default
 class TestBotBasics(unittest.TestCase):
 
     def setUp(self):
-        self.tgbot = Application(retrieve_default()['token'])
+        try:
+            self.tgbot = Application(retrieve_default()['token'])
+        except FileNotFoundError:
+            pass
 
+    @unittest.skipUnless(os.path.exists("./bot/config.ini"), "Configuration file not found")
     def test_if_retrieve_default_works(self):
         self.assertEqual(type(dict(retrieve_default())), type({}))
 
+    @unittest.skipUnless(os.path.exists("./bot/config.ini"), "Configuration file not found")
     def test_if_retrieve_default_has_token(self):
         self.assertTrue('token' in list(retrieve_default()))
 
+    @unittest.skipUnless(os.path.exists("./bot/config.ini"), "Configuration file not found")
     def test_if_retrieve_without_file_raises_error(self):
         with self.assertRaises(FileNotFoundError):
             retrieve_default("fail.ini")
