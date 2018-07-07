32dbad9c844b9fff9a5b73278ba3dcd97564bc05
alexandrebarbaruiva <lexander.santos@gmail.com>
2018-07-07 02:10:21 -0300

    Start linter work

--- tests/test_application.py
+++ tests/test_application.py
@@ -20,15 +20,18 @@ class TestBotBasics(unittest.TestCase):
         except FileNotFoundError:
             pass
 
-    @unittest.skipUnless(os.path.exists("./bot/config.ini"), "Configuration file not found")
+    @unittest.skipUnless(os.path.exists("./bot/config.ini"),
+                         "Configuration file not found")
     def test_if_retrieve_default_works(self):
         self.assertEqual(type(dict(retrieve_default())), type({}))
 
-    @unittest.skipUnless(os.path.exists("./bot/config.ini"), "Configuration file not found")
+    @unittest.skipUnless(os.path.exists("./bot/config.ini"),
+                         "Configuration file not found")
     def test_if_retrieve_default_has_token(self):
         self.assertTrue('token' in list(retrieve_default()))
 
-    @unittest.skipUnless(os.path.exists("./bot/config.ini"), "Configuration file not found")
+    @unittest.skipUnless(os.path.exists("./bot/config.ini"),
+                         "Configuration file not found")
     def test_if_retrieve_without_file_raises_error(self):
         with self.assertRaises(FileNotFoundError):
             retrieve_default("fail.ini")
--- tests/test_linter.py
+++ tests/test_linter.py
@@ -16,8 +16,7 @@ class TestLinter(unittest.TestCase):
 
     def setUp(self):
         self.linter = Linter()
-        self.FILE = "test.yml"
-        self.FILE_PATH = "./bot_test/dialog/tests"
+        self.FILE_PATH = "./bot_test/dialogs"
         os.makedirs(self.FILE_PATH)
 
     def tearDown(self):
@@ -26,37 +25,17 @@ class TestLinter(unittest.TestCase):
 
     def test_if_linter_finds_one_yml(self):
         """
-        Check if linter identifies one yml file on test directory
+        Check if linter identifies json file on test directory with files
         """
-        FILE_TOPIC = "/test"
-        os.makedirs(self.FILE_PATH + FILE_TOPIC)
-        FPATH = "{0}/{1}/{2}".format(self.FILE_PATH, FILE_TOPIC, self.FILE)
+        filename = "dialogs.json"
+        FPATH = "{0}/{1}".format(self.FILE_PATH, filename)
+        with open(FPATH, "w") as test_json:
+            test_json.write("[\"test\"]")
 
-        with open(FPATH, "w") as file_test:
-            file_test.write("Hello World")
-        self.assertEqual(self.linter.check_files(path=self.FILE_PATH), 1)
+        self.assertEqual(self.linter.check_files(path=self.FILE_PATH), 0)
 
         os.remove(FPATH)
-        os.rmdir(self.FILE_PATH + FILE_TOPIC)
 
-    def test_if_linter_finds_one_yml(self):
-        """
-        Check if linter identifies all yml files on test directory
-        """
-        FILE_TOPIC = "/test"
-        os.makedirs(self.FILE_PATH + FILE_TOPIC)
-        FPATH = []
-        FPATH.append("{0}/{1}/{2}".format(self.FILE_PATH, FILE_TOPIC, self.FILE))
-        FPATH.append("{0}/{1}/{2}".format(self.FILE_PATH, FILE_TOPIC , "file.yml"))
-
-        for item in FPATH:
-            with open(item, "w") as file_test:
-                file_test.write("Hello World")
-        self.assertEqual(self.linter.check_files(path=self.FILE_PATH), 2)
-
-        for item in FPATH:
-            os.remove(item)
-        os.rmdir(self.FILE_PATH + FILE_TOPIC)
 
 
 if __name__ == '__main__':
