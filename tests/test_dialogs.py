216a7c04ac6472cf050ab1391faebb140dcf257b
tapumar@gmail.com <tapumar@gmail.com>
2018-08-30 15:02:07 -0100

    fix pep8 style

--- tests/test_dialogs.py
+++ tests/test_dialogs.py
@@ -1,12 +1,13 @@
-import unittest
-import sys
 import os
+import sys
+import unittest
+from bot.communication import Communication
+
 sys.path.append(
     os.path.dirname(
         os.path.dirname(os.path.realpath(__file__))
     )
 )
-from bot.communication import Communication
 
 
 class TestBotDialogs(unittest.TestCase):
@@ -45,7 +46,7 @@ class TestBotDialogs(unittest.TestCase):
                 "Por que o robô foi ao médico? Porque ele tinha vírus!",
                 "Como um robô pirata se chama? Argh2D2.",
                 ("Por que o robô atravessou a rua? " +
-                "Porque ele foi programado por uma galinha"),
+                 "Porque ele foi programado por uma galinha"),
                 "Como você para um robô de destruir o mundo? Você não para."
             ]
         )
@@ -55,112 +56,127 @@ class TestBotDialogs(unittest.TestCase):
         Check if communication answers feelings appropriately
         """
         self.assertIn(
-            self.comm.respond("Eu tenho me sentido bastante deprimido"),
+            self.comm.respond(
+                "Eu tenho me sentido bastante deprimido"),
             [
                 "Qual você acredita ser a causa desse sentimento?",
                 "Em quais situações você costuma se sentir assim?",
                 "Esse sentimento é comum no seu círculo social?",
-                 "Desde quando você tem se sentido assim?"
+                "Desde quando você tem se sentido assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho me sentido com raiva"),
+            self.comm.respond(
+                "Eu tenho me sentido com raiva"),
             [
                 "Qual você acredita ser a causa desse sentimento?",
                 "Em quais situações você costuma se sentir assim?",
                 "Esse sentimento é comum no seu círculo social?",
-                 "Desde quando você tem se sentido assim?"
+                "Desde quando você tem se sentido assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho sentido dores inexplicáveis"),
+            self.comm.respond(
+                "Eu tenho sentido dores inexplicáveis"),
             [
                 "Qual você acredita ser a causa desse sentimento?",
                 "Em quais situações você costuma se sentir assim?",
                 "Esse sentimento é comum no seu círculo social?",
-                 "Desde quando você tem se sentido assim?"
+                "Desde quando você tem se sentido assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho evitado situações que me deixam ansioso"),
+            self.comm.respond(
+                "Eu tenho evitado situações que me deixam ansioso"),
             [
                 "Qual você acredita ser a causa desse comportamento?",
                 "Em quais situações você costuma se comportar assim?",
                 "Esse comportamento é comum no seu círculo social?",
-                 "Desde quando você tem se comportado assim?"
+                "Desde quando você tem se comportado assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho sentido como se eu pudesse ouvir o que outras pessoas pensam"),
+            self.comm.respond(
+                "Eu tenho sentido como se eu pudesse ouvir o que outras " +
+                "pessoas pensam"),
             [
                 "Qual você acredita ser a causa desse sentimento?",
                 "Em quais situações você costuma se sentir assim?",
                 "Esse sentimento é comum no seu círculo social?",
-                 "Desde quando você tem se sentido assim?"
+                "Desde quando você tem se sentido assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho me sentido como se eu não fosse eu mesmo"),
+            self.comm.respond(
+                "Eu tenho me sentido como se eu não fosse eu mesmo"),
             [
                 "Qual você acredita ser a causa desse sentimento?",
                 "Em quais situações você costuma se sentir assim?",
                 "Esse sentimento é comum no seu círculo social?",
-                 "Desde quando você tem se sentido assim?"
+                "Desde quando você tem se sentido assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho começado mais projetos do que costumo fazer"),
+            self.comm.respond(
+                "Eu tenho começado mais projetos do que costumo fazer"),
             [
                 "Qual você acredita ser a causa desse comportamento?",
                 "Em quais situações você costuma se comportar assim?",
                 "Esse comportamento é comum no seu círculo social?",
-                 "Desde quando você tem se comportado assim?"
+                "Desde quando você tem se comportado assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho pensado sobre machucar a mim mesmo"),
+            self.comm.respond(
+                "Eu tenho pensado sobre machucar a mim mesmo"),
             [
                 "Qual você acredita ser a causa desse pensamento?",
                 "Em quais situações você costuma pensar assim?",
                 "Esse pensamento é comum no seu círculo social?",
-                 "Desde quando você tem pensa assim?"
+                "Desde quando você tem pensa assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho tido problemas para dormir que afetam minha qualidade de sono"),
+            self.comm.respond(
+                "Eu tenho tido problemas para dormir que afetam minha " +
+                "qualidade de sono"),
             [
                 "Qual você acredita ser a causa desse problema?",
                 "Esse problema é comum no seu círculo social?",
-                 "Desde quando você tem esse problema?"
+                "Desde quando você tem esse problema?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho tido problemas de memória"),
+            self.comm.respond(
+                "Eu tenho tido problemas de memória"),
             [
                 "Qual você acredita ser a causa desse problema?",
                 "Esse problema é comum no seu círculo social?",
-                 "Desde quando você tem esse problema?",
-                 "Em quais situações você costuma ter esse problema?",
+                "Desde quando você tem esse problema?",
+                "Em quais situações você costuma ter esse problema?",
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho repetido várias vezes"),
+            self.comm.respond(
+                "Eu tenho repetido várias vezes"),
             [
                 "Qual você acredita ser a causa desse comportamento?",
                 "Em quais situações você costuma se comportar assim?",
                 "Esse comportamento é comum no seu círculo social?",
-                 "Desde quando você tem se comportado assim?"
+                "Desde quando você tem se comportado assim?"
             ]
         )
         self.assertIn(
-            self.comm.respond("Eu tenho ingerido muitas vezes"),
+            self.comm.respond(
+                "Eu tenho ingerido muitas vezes"),
             [
                 "Qual você acredita ser a causa desse comportamento?",
                 "Em quais situações você costuma se comportar assim?",
                 "Esse comportamento é comum no seu círculo social?",
-                 "Desde quando você tem se comportado assim?"
+                "Desde quando você tem se comportado assim?"
             ]
         )
+
     def test_if_comm_answers_triggers(self):
         """
         Check if communication answers dialog triggers appropriately
