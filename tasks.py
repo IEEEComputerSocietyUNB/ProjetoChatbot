9cb118bf4753b97dc4cab5100d967337a188999c
vinicius <viniciuscosta0197@gmail.com>
2018-09-06 11:59:31 -0300

    Added new tasks to tasks.py

--- tasks.py
+++ tasks.py
@@ -1,13 +1,38 @@
 from invoke import task
 import os
 
-@task
-def build(c):
-    print("Building!")
+# Name definitions
+first_test = "tests.test_dialogs.TestBotDialogs.test_if_comm_answers_greetings"
+db_file = "db.sqlite3"
+app = "bot/application.py"
+app_test = "tests/test_application.py"
+comm_test = "tests/test_communication.py"
 
 @task
 def rmdb(c):
     """ Removes test database """
-    if os.path.exists("db.sqlite3"):
-        print('hey')
-        os.remove("db.sqlite3")
+    if os.path.exists(db_file):
+        os.remove(db_file)
+
+@task(rmdb)
+def test(c):
+    """ Runs all tests """
+    c.run("green3 " + first_test)
+    c.run("green3 .")
+
+@task
+def style(c):
+    c.run("pycodestyle bot/. tests/. --ignore=E402,W504")
+
+@task
+def doc(c):
+    c.run("pydocstyle bot/. tests/.")
+
+@task(rmdb)
+def run(c):
+    c.run("python3.6 " + app)
+
+@task(rmdb, style, test)
+def travis(c):
+    """ What will run on travis """
+    c.run("invoke rmdb")
